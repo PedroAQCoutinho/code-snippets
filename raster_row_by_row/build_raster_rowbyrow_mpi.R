18f44d74abf39ba61dad7a0881e24f3d5883803b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2022-08-09 10:19:34 -0300

    Update build_raster_rowbyrow_mpi.R

--- raster_row_by_row/build_raster_rowbyrow_mpi.R
+++ raster_row_by_row/build_raster_rowbyrow_mpi.R
@@ -3,48 +3,53 @@ library(snow)
 library(parallel)
 library(Rmpi)
 
+##This code aims to build a big raster row by row due to inssuficient memmory for the operations
+##This is based on MPI parallel processing approach and was used in a cluster with several processing cores
 
+#Load rasters
 r1 <- raster('raster1.tif')
 r2 <- raster('raster.tif')
-
+#Create an alligned output which will be overwrited
 output <- raster(r1)
+#Create blocks
 bss <- blockSize(output, minblocks = 10000)
- 
+
+#Function for table management
 fun_ex <- function(i){
-			db <- data.frame(
-					ras1 = getValues(r1, row = bss$row[i], nrows = bss$nrows[i]),
-					ras2 = getValues(r2, row = bss$row[i], nrows = bss$nrows[i]),
-					out = NA)
-				
+	#Get table
+	db <- data.frame(
+		ras1 = getValues(r1, row = bss$row[i], nrows = bss$nrows[i]),
+		ras2 = getValues(r2, row = bss$row[i], nrows = bss$nrows[i]),
+		out = NA)
 
-			sp <- which(db$ras2==10)
-				
-			if(length(sp) > 0) db[sp, 'out'] <- db[sp, 'ras1']
-
-			return(db$out)
+	#if operations
+	sp <- which(db$ras2==10)
+	if(length(sp) > 0) db[sp, 'out'] <- db[sp, 'ras1']
+	return(db$out)
 			
 }
  
 
 
-
+#Function for exporting results
 exporta <- function(x){
          out <- raster(x)
          cl <- getCluster()
          on.exit(returnCluster())
          nodes <- length(cl)
- 
+ 	 #Put the functions in the nodes
          for(i in 1:nodes){
  			sendCall(cl[[i]], fun_ex, i, tag = i)
          }
  
+	#Start writing
          out <- writeStart(out,
            filename = 'raster_output.tif',
            datatype = 'INT2U',
            format='raster'
         
 		)
- 
+	#Spread the blocks over the processes
          for (i in 1:bss$n) {
 
  			d <- recvOneData(cl)
@@ -70,24 +75,25 @@ exporta <- function(x){
         
 		}
 
- 		#Exporta os dados
+	#Write and export
      out <- writeStop(out)
       return(list(out))
                  
  
  }
  
- #Inicia o cluster de processamento
+ #Cluster start
 cl <- makeCluster(168, type = 'MPI', outfile='')
 options(rasterClusterObject = cl)
 options(rasterClusterCores = length(cl))
 options(rasterCluster = TRUE)
 options(rasterClusterExclude = NULL)
  
- #Carrega bibliotecas no cluster
+ #Load libraries
 clusterEvalQ(cl, library(raster))
 clusterExport(cl=cl,
 	ls())
- 
+
+#Run
 output <- exporta(output)
 stopCluster(cl)
