636e9106e1adffcbbcd5444e809eeef712e71774
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-08-09 10:04:43 -0300

    Prototype for multiprocessing concurrency

--- concurrency/multiprocess_concurrency.py
+++ concurrency/multiprocess_concurrency.py
@@ -1,17 +1,40 @@
 """
 This is a prototype for concurrency based on multiprocessing
+First part ir using the multiprocessing module and the second is using the concurrent.futures
 """
-from asyncio import as_completed
-import concurrent.futures
-import time
-
-start = time.perf_counter()
-
 
 def do_something(seconds):
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
     return f'Done Sleeping...{seconds}'
+
+
+
+import multiprocessing
+import time
+
+start = time.perf_counter()
+
+#this creates several processes
+processes = []
+for i in range(5, 0, -1):
+    p = multiprocessing.Process(target = do_something, args = [i])
+    p.start()
+    processes.append(p)
+
+#This is used to prevent the code to keep foing further before the processes end up the tasks
+for p in processes:
+    p.join()
+
+
+finish = time.perf_counter()
+print(f'First part finished in {round(finish-start, 2)} second(s)')
+
+from asyncio import as_completed
+import concurrent.futures
+import time
+
+start = time.perf_counter()
 
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -23,4 +46,4 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
 
 finish = time.perf_counter()
 
-print(f'Finished in {round(finish-start, 2)} second(s)')
+print(f'Second part finished in {round(finish-start, 2)} second(s)')
