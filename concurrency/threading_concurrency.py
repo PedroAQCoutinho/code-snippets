feb6cfd3e07aff148681419f750a2345e3c06186
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-08-09 10:05:11 -0300

    Prototype for threading  concurrency

--- concurrency/threading_concurrency.py
+++ concurrency/threading_concurrency.py
@@ -1,11 +1,6 @@
 """
 This is a prototype for concurrency based on multiprocessing
 """
-from asyncio import as_completed
-import concurrent.futures
-import time
-
-start = time.perf_counter()
 
 
 def do_something(seconds):
@@ -13,6 +8,35 @@ def do_something(seconds):
     time.sleep(seconds)
     return f'Done Sleeping...{seconds}'
 
+
+
+import threading
+import time
+
+start = time.perf_counter()
+
+#this creates several processes
+processes = []
+for i in range(5, 0, -1):
+    p = threading.Thread(target = do_something, args = [i])
+    p.start()
+    processes.append(p)
+
+#This is used to prevent the code to keep foing further before the processes end up the tasks
+for p in processes:
+    p.join()
+
+
+finish = time.perf_counter()
+print(f'First part finished in {round(finish-start, 2)} second(s)')
+
+
+
+from asyncio import as_completed
+import concurrent.futures
+import time
+
+start = time.perf_counter()
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5, 4, 3, 2, 1]
