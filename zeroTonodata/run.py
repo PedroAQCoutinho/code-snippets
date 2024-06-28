2f72c136f54230a228eb8034ef38d1ac83f51999
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2024-06-28 10:36:36 -0300

    Create run.py
    Script para colocar NA no lugar de zeros

---
+++ zeroTonodata/run.py
@@ -0,0 +1,27 @@
+import rasterio
+import numpy as np
+
+# Defina o caminho para o arquivo raster original e o novo arquivo raster
+input_raster_path = '/home/pedro/Documents/DADOS/pa_br_aptidao_250m.tif'
+output_raster_path = '/home/pedro/Documents/DADOS/pa_br_aptidao_250m_zndt.tif'
+
+# Valor nulo (nodata) a ser usado no novo raster
+nodata_value = 0
+
+# Abre o arquivo raster original
+with rasterio.open(input_raster_path) as src:
+    # Lê os dados da primeira banda do raster
+    data = src.read(1)
+    
+    # Substitui os valores 0 pelo valor nulo (nodata_value)
+    data = np.where(data == 0, nodata_value, data)
+    
+    # Cria um novo perfil baseado no perfil original, incluindo o valor nulo (nodata)
+    profile = src.profile
+    profile.update(nodata=nodata_value)
+    
+    # Cria um novo arquivo raster com os dados modificados
+    with rasterio.open(output_raster_path, 'w', **profile) as dst:
+        dst.write(data, 1)
+
+print(f"Novo arquivo raster criado em: {output_raster_path}")
