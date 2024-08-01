d3f27ac15f1d16bc807482ad641fec12ce4231c3
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2024-08-01 09:55:50 -0300

    Update run.R

--- config_env_files/run.R
+++ config_env_files/run.R
@@ -1,13 +1,14 @@
 library(jsonlite)
-library(dotenv)
 
+
+rm(list=ls())
 ############
 #CONFIG FILE
 ############
 
 # Lendo o arquivo de configuração​
 
-config <- fromJSON("config_env_files/config.json")
+config <- fromJSON("config.json")
 
 paths <- config$paths
 ckan_recursos <- config$ckan_recursos
@@ -15,25 +16,46 @@ salvar_dados <- config$salvar_dados
 auxiliares <- config$auxiliares
 arquivos <- config$arquivos
 
-source(paths$ckan_utils_path)
+
+#Verificar se conseguimos ler um link
+source('https://github.com/PedroAQCoutinho/code-snippets/blob/main/config_env_files/ckan_utils.R')
 source(paths$biblioteca_path)
 
 
 #leitura de arquivos
 r1 <- raster(paste(paths$dados_path, arquivos$malha, sep = '/'))
 
-
 cl <- makeCluster(auxiliares$num_nucleos)
 
 
 
+
+
+
+
+library(dplyr)
+library(dotenv)
+
+
+rm(list=ls())
+
+
+
+
+#Ler arquivos
+
+#Fazer conexoes
+load_dot_env() #Sistema operacional ja pussui a var armazenada
+source('ckan_utils.r')
+ckan <- init_ckanr() #Le a var armazenada no sistema
 
 ############
 #ENV FILE
 ############
 
 
-dotenv::load_dot_env()
+
+
 
 db_name <- Sys.getenv('DB_NAME')
 db_password <- Sys.getenv('DB_PASSWORD')
@@ -42,13 +64,15 @@ db_user <- Sys.getenv('DB_USER')
 db_host <- Sys.getenv('DB_HOST')
 
 
+
+
 con <- dbConnect(
   RPostgres::Postgres(),
   dbname = db_name,
   host = db_host, 
   port = db_port,       
   user = db_user,
-  password = db_password
+  password = Sys.getenv('DB_PASSWORD')
 )
 
 
