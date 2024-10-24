d601b1c39aa28777a7f6cafc61268b8bfecbddc1
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2024-10-24 16:35:12 -0300

    Update run.R

--- config_env_files/run.R
+++ config_env_files/run.R
@@ -1,14 +1,21 @@
 library(jsonlite)
+library(dotenv)
 
 
-rm(list=ls())
+
+source("https://raw.githubusercontent.com/PedroAQCoutinho/code-snippets/main/config_env_files/ckan_utils.R")
+
+
+
+# rm(list=ls())
 ############
 #CONFIG FILE
 ############
 
-# Lendo o arquivo de configuração​
-
+# Lendo o arquivo de configuração
 config <- fromJSON("config.json")
+
+
 
 paths <- config$paths
 ckan_recursos <- config$ckan_recursos
@@ -16,46 +23,12 @@ salvar_dados <- config$salvar_dados
 auxiliares <- config$auxiliares
 arquivos <- config$arquivos
 
-
-#Verificar se conseguimos ler um link
-source('https://github.com/PedroAQCoutinho/code-snippets/blob/main/config_env_files/ckan_utils.R')
-source(paths$biblioteca_path)
-
-
-#leitura de arquivos
-r1 <- raster(paste(paths$dados_path, arquivos$malha, sep = '/'))
-
-cl <- makeCluster(auxiliares$num_nucleos)
-
-
-
-
-
-
-
-library(dplyr)
-library(dotenv)
-
-
-rm(list=ls())
-
-
-
-
-#Ler arquivos
-
 #Fazer conexoes
-load_dot_env() #Sistema operacional ja pussui a var armazenada
-source('ckan_utils.r')
-ckan <- init_ckanr() #Le a var armazenada no sistema
+load_dot_env(file = '.env') #Sistema operacional ja pussui a var armazenada
 
 ############
 #ENV FILE
 ############
-
-
-
-
 
 db_name <- Sys.getenv('DB_NAME')
 db_password <- Sys.getenv('DB_PASSWORD')
@@ -65,20 +38,6 @@ db_host <- Sys.getenv('DB_HOST')
 
 
 
-
-con <- dbConnect(
-  RPostgres::Postgres(),
-  dbname = db_name,
-  host = db_host, 
-  port = db_port,       
-  user = db_user,
-  password = Sys.getenv('DB_PASSWORD')
-)
-
-
-#Dentro do código init_ckanr() há um trecho que faz
-#Sys.getenv('CKAN_KEY') e ussa essa key para fazer autenticação
-ckan <- init_ckanr()
 
 
 
